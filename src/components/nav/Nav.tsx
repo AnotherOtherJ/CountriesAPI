c14f7df337fc153640e1b6add4a5a33dd36c30b4
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-09-12 23:10:32 +0200

    Fixed letter

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -3,7 +3,7 @@ import StyledNav from "./Nav.style";
 const Nav = (): JSX.Element => {
   return (
     <StyledNav>
-      <h1>Were in the world!</h1>
+      <h1>Were in the world?</h1>
     </StyledNav>
   );
 };
