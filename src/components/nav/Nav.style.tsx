54086561629eda1cfced38d22427af64584274a9
AnotherOtherJ <kuba2323kuba@gmail.com>
2022-09-13 15:53:53 +0200

    fixed nav 	modified:   src/components/nav/Nav.style.tsx 	modified:   src/components/nav/Nav.tsx

--- src/components/nav/Nav.style.tsx
+++ src/components/nav/Nav.style.tsx
@@ -5,7 +5,7 @@ const StyledNav = styled.nav`
   align-items: center;
   justify-content: space-between;
   flex: 0 0 80px;
-  padding: 0 clamp(20px, 5vw, 80px);
+  padding: 0 clamp(20px, 6vw, 80px);
   background-color: ${({ theme: { element } }) => element};
 `;
 
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -3,7 +3,8 @@ import StyledNav from "./Nav.style";
 const Nav = (): JSX.Element => {
   return (
     <StyledNav>
-      <h1>Were in the world!</h1>
+      <h1>Were in the world?</h1>
+      <div>Dark mode</div>
     </StyledNav>
   );
 };
