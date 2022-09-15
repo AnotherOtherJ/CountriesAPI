e2a96094c8018461ac4069e93f50178c973cebd2
AnotherOtherJ <kuba2323kuba@gmail.com>
2022-09-15 19:04:06 +0200

    modified:   src/pages/countriesGrid/CountriesGrid.tsx

--- src/pages/countriesGrid/CountriesGrid.tsx
+++ src/pages/countriesGrid/CountriesGrid.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+
 import CountryCard from "@/components/countryCard/CountryCard";
 import Filter from "@/components/filter/Filter";
 import PageWrapper from "@/components/pageWrapper/PageWrapper";
@@ -7,8 +9,25 @@ import useCountries from "@/hooks/useCountries";
 import { StyledHeader, StyledSection } from "./CountriesGrid.style";
 
 const CountriesGrid = (): JSX.Element => {
-  const { isLoading, error, data, isSuccess } = useCountries();
+  const { isLoading, error, data } = useCountries();
   console.log(data);
+
+  const countries = useMemo(
+    () =>
+      data &&
+      data.map(country => (
+        <CountryCard
+          key={country.name.common}
+          flag={country.flags.svg}
+          countryName={country.name.common}
+          population={country.population}
+          capital={country.capital}
+          continent={country.continents}
+        />
+      )),
+
+    [data],
+  );
 
   return (
     <PageWrapper>
@@ -22,17 +41,7 @@ const CountriesGrid = (): JSX.Element => {
         ) : error instanceof Error ? (
           <div>there was an error {error.message}</div>
         ) : (
-          isSuccess &&
-          data.map(country => (
-            <CountryCard
-              key={country.name.common}
-              flag={country.flags.svg}
-              countryName={country.name.common}
-              population={country.population}
-              capital={country.capital}
-              continent={country.continents}
-            />
-          ))
+          countries
         )}
       </StyledSection>
     </PageWrapper>
