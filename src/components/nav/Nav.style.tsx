d18072266e40c59d3135b18ca0ddd62addf11179
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-09-15 20:04:04 +0200

    Fixed nav font

--- src/components/nav/Nav.style.tsx
+++ src/components/nav/Nav.style.tsx
@@ -5,6 +5,7 @@ const StyledNav = styled.nav`
   align-items: center;
   justify-content: space-between;
   flex: 0 0 80px;
+  font-size: 1.1rem;
   padding: 0 clamp(20px, 6vw, 80px);
   background-color: ${({ theme: { element } }) => element};
 `;
