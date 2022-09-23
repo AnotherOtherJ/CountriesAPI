022d7438da4f1f7d5c10710a3fefadfb19d5b1cd
AnotherOtherJ <kuba2323kuba@gmail.com>
2022-09-24 01:26:18 +0200

    added boxshadows 	modified:   src/components/button/Button.tsx 	modified:   src/components/countryCard/CountryCard.style.tsx

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -6,8 +6,7 @@ const StyledButton = styled.div`
   justify-content: center;
   gap: 10px;
   max-width: 130px;
-  padding: 0 25px;
-  height: 30px;
+  padding: 3px 20px;
   background-color: ${({ theme: { element } }) => element};
   border-radius: 3px;
   box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
--- src/components/countryCard/CountryCard.style.tsx
+++ src/components/countryCard/CountryCard.style.tsx
@@ -8,6 +8,7 @@ const StyledCard = styled.div`
   width: 270px;
   height: 340px;
   background-color: ${({ theme: { element } }) => element};
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
   img {
     min-height: 160px;
     object-fit: cover;
