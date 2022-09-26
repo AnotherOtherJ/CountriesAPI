4eb27f876761f7642a1977ae2f5d70e661783373
AnotherOtherJ <kuba2323kuba@gmail.com>
2022-09-26 20:13:17 +0200

    deleted old code 	modified:   src/components/countriesGrid/CountriesGrid.style.tsx

--- src/components/countriesGrid/CountriesGrid.style.tsx
+++ src/components/countriesGrid/CountriesGrid.style.tsx
@@ -17,19 +17,4 @@ export const StyledSection = styled(m.main)`
   gap: clamp(20px, 11vw, 60px);
   justify-content: center;
   padding-top: clamp(40px, 6vw, 50px);
-  /* display: flex;
-  flex-wrap: wrap;
-  gap: clamp(20px, 11vw, 60px);
-  align-items: center;
-  justify-content: center;
-  padding-top: clamp(40px, 6vw, 50px); */
-  /* margin-top: clamp(40px, 6vw, 50px);
-  width: 100%;
-  height: 100%;
-  display: grid;
-  justify-content: center;
-  grid-row-gap: 65px;
-  grid-column-gap: 65px;
-  grid-template-columns: repeat(auto-fill, 270px);
-  grid-template-rows: repeat(auto-fill, 340px); */
 `;
