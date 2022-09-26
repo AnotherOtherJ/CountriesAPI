a2601f5b50ecfa3869ab28ee28c000e2b99bdceb
AnotherOtherJ <kuba2323kuba@gmail.com>
2022-09-26 03:38:58 +0200

    Fixes     Fixed Image flickering on chromium     Changed layout back to grid 	modified:   src/components/countriesGrid/CountriesGrid.style.tsx 	modified:   src/components/countryCard/CountryCard.style.tsx

--- src/components/countriesGrid/CountriesGrid.style.tsx
+++ src/components/countriesGrid/CountriesGrid.style.tsx
@@ -6,19 +6,23 @@ export const StyledHeader = styled.header`
   justify-content: space-between;
   gap: 5px;
   flex-wrap: wrap;
-  width: 100%;
   @media screen and (max-width: 480px) {
     gap: 40px;
   }
 `;
 
 export const StyledSection = styled(m.main)`
-  display: flex;
+  display: grid;
+  grid: repeat(auto-fit, 340px) / repeat(auto-fit, 270px);
+  gap: clamp(20px, 11vw, 60px);
+  justify-content: center;
+  padding-top: clamp(40px, 6vw, 50px);
+  /* display: flex;
   flex-wrap: wrap;
   gap: clamp(20px, 11vw, 60px);
   align-items: center;
   justify-content: center;
-  padding-top: clamp(40px, 6vw, 50px);
+  padding-top: clamp(40px, 6vw, 50px); */
   /* margin-top: clamp(40px, 6vw, 50px);
   width: 100%;
   height: 100%;
--- src/components/countryCard/CountryCard.style.tsx
+++ src/components/countryCard/CountryCard.style.tsx
@@ -6,12 +6,12 @@ const StyledCard = styled(motion.div)`
   flex-direction: column;
   border-radius: 5px;
   overflow: hidden;
-  width: 270px;
-  height: 340px;
   background-color: ${({ theme: { element } }) => element};
   box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
   img {
     min-height: 160px;
+    backface-visibility: hidden;
+    transform: translateZ(0);
     object-fit: cover;
   }
   div {
